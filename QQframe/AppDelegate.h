21132291510f07be83b2b7e7f63c48a7f8225184
(外包) 钟文成 <ex-zhongwencheng001@zhongwenheng.local>
2016-08-31 11:10:18 +0800

    Initial Commit

---
+++ QQframe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQframe
+//
+//  Created by 钟文成(外包) on 16/8/31.
+//  Copyright © 2016年 钟文成(外包). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QQframe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QQframe
+//
+//  Created by 钟文成(外包) on 16/8/31.
+//  Copyright © 2016年 钟文成(外包). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
